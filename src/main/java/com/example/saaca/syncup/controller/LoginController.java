9d2729f172a7d4629649f6fad88d5e1c52c60bc9
vipul goyal <vipulgoyal794@gmail.com>
2020-06-21 13:03:33 +0530

    heroku changes

--- src/main/java/com/example/saaca/syncup/controller/LoginController.java
+++ src/main/java/com/example/saaca/syncup/controller/LoginController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/login")
-@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
+@CrossOrigin(origins = "https://syncup-angular-client.herokuapp.com", maxAge = 3600)
 public class LoginController {
 
     @Autowired
