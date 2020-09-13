f6ca0f64a27eb1acb3b7a0de74c7ae1104bb5e9b
vipul goyal <vipulgoyal794@gmail.com>
2020-09-13 13:12:42 +0530

    changed back url

--- src/main/java/com/example/saaca/syncup/SimpleCORSFilter.java
+++ src/main/java/com/example/saaca/syncup/SimpleCORSFilter.java
@@ -5,11 +5,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Component
+@WebFilter("/api/v1/*")
 public class SimpleCORSFilter implements Filter {
 
     private final Logger log = LoggerFactory.getLogger(SimpleCORSFilter.class);
